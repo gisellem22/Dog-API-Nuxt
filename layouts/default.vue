8522b2e4d584679791fdecce1056b0dc50ec878f
Giselle Machado <gisellemachadoyes@gmail.com>
2022-04-09 09:46:27 -0400

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app light>
+      <Nuxt />
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {};
+  },
+};
+</script>
